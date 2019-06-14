dc3064d2acb11bdbc2868f32fe0b787754e94470
Cynthia Higginbotham <cchjob@republicofnewhome.org>
2019-06-13 20:13:25 -0500

    updated build.gradle, moved package to mod.alexndr.simpleores

--- src/main/java/mod/alexndr/simpleores/SimpleOres.java
+++ src/main/java/mod/alexndr/simpleores/SimpleOres.java
@@ -1,4 +1,4 @@
-package alexndr.simpleores;
+package mod.alexndr.simpleores;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,9 +19,12 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("simpleores")
+@Mod(SimpleOres.MODID)
 public class SimpleOres
 {
+	// modid 
+	public static final String MODID = "simpleores";
+	
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
