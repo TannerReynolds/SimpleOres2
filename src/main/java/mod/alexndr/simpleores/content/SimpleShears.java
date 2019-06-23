187e7af2900064098ca9228804e3a2fa4b4ba3fb
Cynthia Higginbotham <cchjob@republicofnewhome.org>
2019-06-23 09:48:11 -0500

    added SimpleShears class, so I could work around defects in ShearsItem class

---
+++ src/main/java/mod/alexndr/simpleores/content/SimpleShears.java
@@ -0,0 +1,13 @@
+package mod.alexndr.simpleores.content;
+
+import net.minecraft.item.ShearsItem;
+
+public class SimpleShears extends ShearsItem 
+{
+
+	public SimpleShears(Properties builder) {
+		super(builder);
+		// TODO Auto-generated constructor stub
+	}
+
+} // end class SimpleShears
